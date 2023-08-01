e429943a7771fac020083da91aa84e665c8dcd7a
maulanasidik <maulanasidik@gmail.com>
2023-08-02 00:10:18 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,18 +5,19 @@ yum install yum-utils wget curl nano htop -y
 yum remove git git-* *-git -y
 
 # git latest
-sudo yum -y install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm
-sudo yum install git -y
+yum -y install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm
+yum install git -y
 
-sudo su
+
 cd
 cd /etc/yum.repos.d/
-sudo wget -O /etc/yum.repos.d/shells:fish:release:3.repo --no-check-certificate https://download.opensuse.org/repositories/shells:fish:release:3/CentOS_7/shells:fish:release:3.repo
-sudo yum install fish -y 
+wget -O /etc/yum.repos.d/shells:fish:release:3.repo --no-check-certificate https://download.opensuse.org/repositories/shells:fish:release:3/CentOS_7/shells:fish:release:3.repo
+yum install fish -y 
 # oh my fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 # default install /usr/bin/fish
 # https://techviewleo.com/configure-fish-shell-with-oh-my-fish/
+cd
 
 #2 line theme commmadn fish 
 omf install ays
